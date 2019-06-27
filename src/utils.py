babaa20f0ba43006aa5627a03dbb14214c00de29
0xzayd <hamdi.zayd@gmail.com>
2019-06-27 11:42:10 +0200

    New files

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,59 @@
+import numpy as np
+
 
 def make_trainable(net, val):
     for l in net.layers:
         l.trainable = val
 
+def ary_to_tiles_forward(padded_ary, Sary, pad=(3,3), shape=(122,122), exclude_empty=False): # updated to avoid artefacts
+
+    assert(isinstance(padded_ary, np.ndarray))
+    assert(isinstance(Sary, np.ndarray))
+    assert(isinstance(shape, tuple))
+    
+    ary_height = shape[0] + 2*pad[0]
+    ary_width = shape[1] + 2*pad[1]
+    ary_list = [] 
+    
+    total = 0
+    excluded = 0
+    sx = -shape[1] - pad[1]
+    for _ in range(0, Sary.shape[1]//shape[1]+min(1,(Sary.shape[1]%shape[1]))):
+        sx = max(0, sx + ary_width - 2*pad[0])
+        ex = sx + ary_width
+        sy = -shape[0] - pad[0]
+        for _ in range(0, Sary.shape[0]//shape[0]+min(1,(Sary.shape[1]%shape[1]))):           
+            sy = max(0, sy + ary_height - 2*pad[1])
+            ey = sy + ary_height
+            
+            crop_ary = padded_ary[sy:ey,sx:ex]
+
+            ary_list.append(crop_ary)
+    return np.stack(ary_list), excluded
+
+
+def tiles_to_ary_forward(stacked_ary, pad=(3,3), padded_ary_shape_padded=(10986, 10986)):
+    
+    assert(len(stacked_ary.shape) == 4)
+    
+    output_ary_height = padded_ary_shape_padded[0] - 2*pad[0]
+    output_ary_width = padded_ary_shape_padded[1] - 2*pad[1]
+    
+    padded_tile_height = stacked_ary.shape[1]
+    padded_tile_width = stacked_ary.shape[2]
+    
+    
+    output_ary = np.zeros(shape=(output_ary_height,output_ary_width)+(stacked_ary.shape[3],))
+    
+    output_content = stacked_ary[:,pad[1]:-pad[1],pad[0]:-pad[0],:]
+    
+    index = 0
+    for x_step in range(0, output_ary_width, padded_tile_width-2*pad[1]):
+        for y_step in range(0, output_ary_height, padded_tile_height-2*pad[0]):
+            x0, x1 = x_step, x_step+padded_tile_width-2*pad[1]
+            y0, y1 = y_step, y_step+padded_tile_height-2*pad[0]
+            
+            output_ary[y0:y1, x0:x1] = output_content[index]
+            index += 1
+    
+    return output_ary
--- train.py
+++ train.py
@@ -2,55 +2,43 @@ import argparse
 
 from src.models import *
 from src.networks import *
-
-
-import glob
-import os
-
-import numpy as np
-
-import skimage
+from src.data import Data
 
 parser = argparse.ArgumentParser(description='arguments input')
-parser.add_argument('-p','--pan', type=str, help='pan folder', required=True)
-parser.add_argument('-d', '--dsm', type=str, help='dsm folder', required=True)
-parser.add_argument('-l', '--label', type=str, help='label folder', required=True)
+parser.add_argument('-d','--data', type=str, help='Training data folder with subfolders: DSM, PAN, LABEL', required=True)
+
 
 args = parser.parse_args()
-
-# Please process all input images into small tiles of 256x256 pixels tif images
-# (pan, dsm and binary label image) as shown in the example
 
 img_height = 256
 img_width = 256
 
-pan_folder = args.pan
-dsm_folder = args.dsm
-label_folder = args.label
+data_folder = args.data
 
-pan = []
-dsm = []
-label = []
 
-pan = [skimage.io.imread(p) for p in sorted(glob.glob(pan_folder + '/*.tif'))]
-dsm = [skimage.io.imread(d) for d in sorted(glob.glob(dsm_folder + '/*.tif'))]
-label = [skimage.io.imread(l) for l in sorted(glob.glob(label_folder + '/*.tif'))]
+data = Data(data_folder)
 
-pan_input = np.array(pan)
-dsm_input = np.array(dsm)
-label_input = np.array(label)
+# split to train, validation, test
+dsm_train, pan_train, label_train, dsm_vld, pan_vld, label_vld, dsm_tst, pan_tst, label_tst = data.split_trn_vld_tst()
+
+# create two generators: for training and for validation
+train_gen = DataGenerator(dsm_train, pan_train, label_train, pred_fn=None, batch_size=8, 
+                          shuffle=True)
+valid_gen = DataGenerator(dsm_vld, pan_vld, label_vld, pred_fn=None, batch_size=8, 
+                          shuffle=True)
 
 # training
 myModel = Wnet_cgan(img_height, img_width, n_labels=1)
-myModel.build_wnet_cgan([64,64,64,64],
+myModel.build_wnet_cgan([64,64,128,128],
                         (3,3), 
                         wnet_activation='selu',
                         wnet_lr=1e-4,
                         discr_inp_channels = 16,
-                        discr_block_list=[32,32,32,32],
+                        discr_block_list=[32,32,64,64],
                         discr_k_size=(3,3), 
                         discr_activation='relu',
                         discr_lr=1e-4,
                         lambda_=1e-1)
 
-myModel.fit_wnet_cgan(X=[dsm,pan,label], Y=[label,np.ones(shape=(len(label),1))])
+myModel.fit_wnet_cgan(train_gen, valid_gen, adv_epochs=50, gen_epochs=100,
+                     adv_steps_epoch=50, gen_steps_epoch=100, n_rounds=50)
